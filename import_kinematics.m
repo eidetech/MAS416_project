b32c586f62769325d5a404872e97ef682e613217
PerHenrikHardeberg <perhenrikh@hotmail.com>
2022-11-08 15:07:59 +0100

    added real model ant import script. Works to read out angles, but not tried to actuate

---
+++ import_kinematics.m
@@ -0,0 +1,6 @@
+% Dette scriptet må kjøres etter hand of god modellen er kjørt for å
+% importere riktig data. Dataen må lagres som timeseries i simulink
+
+importdata kinematics_test.mat
+
+myData = out.robot_kinematics;
