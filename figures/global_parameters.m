b8649332941716f40de1b1c02391ff1d50f3a508
Marcus Eide <marcuseide@me.com>
2022-11-23 16:12:31 +0100

    Added global parameters

---
+++ figures/global_parameters.m
@@ -0,0 +1,28 @@
+%% Global plotting parameters
+
+% Font sizes
+general_font_size = 11
+title_font_size = 14
+font = 'Times New Roman'
+
+% Combined plot (1x2) will have this name, the two separate plots will have
+% extension _sep_x.eps
+filename = 'omega_A_plot';
+
+% Output path for .eps file
+output_path = ''
+
+% H & W for the figure
+W_frame = 17
+% W_frame = 36;           % Final width of the frame [cm] of the plot in your document
+% H_frame = 7;            % Final height of the frame [cm] of the plot in your document
+H_frame = 6;
+% Number of rows and columns for the figure (for subplots)
+rows = 1;
+cols = 2;
+
+% Select thin or thick linewidth
+LW_thin = true
+
+top_margin = 0.4;
+bottom_margin = 0.1;
