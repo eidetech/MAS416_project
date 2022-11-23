4cf6171c27e61483e3dd99c51928157c8fd9ce32
Marcus Eide <marcuseide@me.com>
2022-11-23 13:02:50 +0100

    Added figure code and eps

--- figures/omega_A_plot.m
+++ figures/omega_A_plot.m
@@ -1,6 +1,6 @@
 clc; close all;
 
-filename = 'all_torques_plot';
+filename = 'omega_A_plot';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if(LW_thin)
@@ -16,17 +16,13 @@ fig.Units = 'centimeters';
 fig.Position = [LLC_frame W_frame/2 H_frame]; %Specifying the width and height of the frame
 hold on
 
-plot(torques{1}.Values, 'LineWidth', LW)
+plot(omegaA{1}.Values, 'LineWidth', LW)
 hold on
-plot(torques{2}.Values, 'LineWidth', LW)
-plot(torques{3}.Values, 'LineWidth', LW)
-plot(torques{4}.Values, '-s', 'MarkerIndices',1:300:length(torques{4}.Values.Data), 'LineWidth', LW)
-plot(torques{5}.Values, '--', 'LineWidth', LW)
-plot(torques{6}.Values, '-o', 'MarkerIndices',1:300:length(torques{4}.Values.Data), 'LineWidth', LW)
-legend('$M_{JA}$', '$M_{JB}$', '$M_{JB2}$', '$M_{JH}$', '$M_{JI}$', '$M_{JG}$', 'Interpreter', 'latex')
+plot(omegaA{2}.Values, 'LineWidth', LW)
+legend('$\omega_{A_{ref}}$', '$\omega_{A}$', 'Interpreter', 'latex')
 grid on;
 xlabel('$t$ [s]', 'interpreter', 'latex')
-ylabel('[Nm]', 'interpreter', 'latex')
+ylabel('[rad/s]', 'interpreter', 'latex')
 title('')
 box on;
 set(gca, 'FontName', font)
